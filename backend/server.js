34e38b52ac39774fc2d0f8134fe4044848e20932
Rajeswari92 <raji032019@gmail.com>
2024-11-30 23:21:21 +0530

    Update code on backend

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(express.json()); // For parsing JSON requests
 app.use('/api', taskRoutes); // Prefix the routes with '/api'
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, {})
 .then(() => console.log('Connected to MongoDB successfully'))
 .catch((err) => console.error('Failed to connect to MongoDB:', err));
 
